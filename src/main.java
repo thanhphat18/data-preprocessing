cec82de4ab698ddc7691df9767b3d07e5b3eae73
Thanh Phat <thanhphatchau@Thanhs-MacBook-Pro.local>
2025-04-19 18:36:59 +0700

    finally, load the csv file :)

--- src/main.java
+++ src/main.java
@@ -1,6 +1,18 @@
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import java.io.File;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try{
+            System.out.println("Loading the CSV file...");
+            String filePath = "src/datasets/output.csv";
+            CSVLoader loader = new CSVLoader();
+            loader.setSource(new File(filePath));
+            Instances data = loader.getDataSet();
+        } catch (Exception e) {
+            System.out.println("Error loading the CSV file: " + e.getMessage());
+        }
     }
     
 }
